c83f1a03aa8ea04e6a35c17b5e454422e2fd8023
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2024-04-01 23:15:32 -0300

    Add CourseResolver interface and implement Category method

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,7 +6,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/josenaldo/fc-graphql-jom/graph/model"
 )
@@ -26,11 +25,25 @@ func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]
 			ID:          course.ID,
 			Name:        course.Name,
 			Description: &course.Description,
-			Category:    obj,
 		})
 	}
 
 	return courseModels, nil
+}
+
+// Category is the resolver for the category field.
+func (r *courseResolver) Category(ctx context.Context, obj *model.Course) (*model.Category, error) {
+	category, error := r.CategoryDB.FindByCourseID(obj.ID)
+
+	if error != nil {
+		return nil, error
+	}
+
+	return &model.Category{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: &category.Description,
+	}, nil
 }
 
 // CreateCategory is the resolver for the createCategory field.
@@ -106,6 +119,9 @@ func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
 // Category returns CategoryResolver implementation.
 func (r *Resolver) Category() CategoryResolver { return &categoryResolver{r} }
 
+// Course returns CourseResolver implementation.
+func (r *Resolver) Course() CourseResolver { return &courseResolver{r} }
+
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
@@ -113,15 +129,6 @@ func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
 type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *categoryResolver) Course(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented: Course - course"))
-}
